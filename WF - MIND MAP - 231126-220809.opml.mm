7bfecaa3c664c2762627f9e1289cdacf31be366c
olafbond <52509158+olafbond@users.noreply.github.com>
2023-11-26 22:23:16 +0300

    Add files via upload

---
+++ WF - MIND MAP - 231126-220809.opml.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<node ID="1" TEXT="MIND MAP" >
+<node ID="2" TEXT="child 1" POSITION="left" >
+<node ID="3" TEXT="grandchild 11" >
+<node ID="4" TEXT="grandgrandchild 111" />
+</node>
+<node ID="5" TEXT="grandchild 12" />
+<node ID="6" TEXT="grandchild 13" />
+</node>
+<node ID="7" TEXT="child 2" POSITION="right" >
+<node ID="8" TEXT="grandchild 21" />
+<node ID="9" TEXT="grandchild 22" />
+</node>
+<node ID="10" TEXT="child 3" POSITION="right" >
+<node ID="11" TEXT="grandchild 31" />
+</node>
+<node ID="12" TEXT="child 4" POSITION="right" >
+<node ID="13" TEXT="grandchild 41" >
+<node ID="14" TEXT="grandgrandchild 411" />
+<node ID="15" TEXT="grandgrandchild 412" />
+</node>
+<node ID="16" TEXT="grandchild 42" />
+<node ID="17" TEXT="grandchild 43" />
+</node>
+<node ID="18" TEXT="child 5" POSITION="left" >
+<node ID="19" TEXT="grandchild 51" />
+<node ID="20" TEXT="grandchild 52" />
+</node>
+</node>
+</map>
